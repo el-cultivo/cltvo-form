6c5c7f2f9414226dd7d434e5e4fabbdf70809e04
Eduardo <developer.sr@elcultivo.mx>
2015-03-11 13:31:51 -0600

    Integración de Mailchimp -código sin probar-

--- ajax/ajax-mail.php
+++ ajax/ajax-mail.php
@@ -2,17 +2,22 @@
 
 include_once 'ajax-mail_config.php';
 
-//----------------------------envio de los correos --------------------------------
+//----------------------------envió de los correos --------------------------------
 
 
-if(isset( $_POST[id_form]) ){ // en esta función se envía la información  
+if( isset($_POST[id_form]) && !isset($break_form) ){ // en esta función se envía la información  solo en caso de que la información llegue correctamente 
 	$datos = nl2br(isarray($_POST[id_form],primera_linea.'<br>')); // nombre del form para crear la cadena 
-	echo cltvo_manda_mail( reg_name, reg_mail, $datos ); // requiere conocer los input de nombre y mail 
+	if(!isset($mailchip_merge_array)){
+		$mailchip_merge_array= array();
+	}
+	echo cltvo_manda_mail( reg_name, reg_mail, $datos , $mailchip_merge_array); // requiere conocer los input de nombre y mail 
+}else{ // o regresa un error en caso de que exista un problema con los form 
+	echo "Error en envío"; 
 }
 
+//----------------------------funciones para el envió del correo --------------------------------
 
 function isarray($variable,$first_line){ // genera el texto del correo a partir de la cadena enviada por el form 
-
 	if(!empty($variable)){
 		if(is_array($variable)){
 				$salida=$first_line; // escribe la primer linea 
@@ -29,7 +34,7 @@ function isarray($variable,$first_line){ // genera el texto del correo a partir 
 }
 
 
-function cltvo_manda_mail($de_quien, $de_quien_mail, $qui_hubo){ // envía correo de registro y notificación 
+function cltvo_manda_mail($de_quien, $de_quien_mail, $qui_hubo, $mailchip_merge_array){ // envía correo de registro y notificación 
 	$pa_donde = mail;
 	$qui_hubo_asunto = "Nuevo registro: ". $de_quien_mail;
 	
@@ -41,7 +46,7 @@ function cltvo_manda_mail($de_quien, $de_quien_mail, $qui_hubo){ // envía corre
 	$qui_hubo_msj  = $qui_hubo;
 	
 	/*---------------------------------envió del mail de registro ----------------------------------------------------------*/
-	$from = "FROM: $de_quien_mail\r\n";
+	$from = "FROM: ".$de_quien_mail."\r\n";
 	$cabeza = "MIME-Version: 1.0\r\n";
 	$cabeza .= "Content-Type: text/html; charset=UTF-8\r\n";
 
@@ -54,33 +59,102 @@ function cltvo_manda_mail($de_quien, $de_quien_mail, $qui_hubo){ // envía corre
 
 	$segundo_mail = mail($de_quien_mail, $asunto, $mensaje, $cabeza);
 	
-	/*---------------------------------regreso de información vía ajax----------------------------------------------------------*/
+	/*---------------------------------regreso de información vía ajax y activación del mailchimp----------------------------------------------------------*/
 	
-	if( $primer_mail && $segundo_mail ){
-		//mailchimp($de_quien_mail, $cabeza); // / función desestabilizada temporalmente 
-		return '¡Gracias!';
+	if( $primer_mail && $segundo_mail ){	
+		if mailchip == true ){ // solo en caso de que quieran registrarse en mailchimp
+			
+			$mergevars_val=array(); // inicia si campos extra en para mailchimp
+			if(mailchip_mergevar_on==true){ // solo en caso de que quieran registrarse campos extra en mailchimp
+				$mailchimp_mergevars=merge_vars_gen($_POST[id_form]);// función para definir el valor de los campos extra
+
+				foreach ($mailchip_merge_array as $key => $value) { // asigna el valor correspondiente a campo extra 
+					if( isset( $mailchimp_mergevars[$value] ){
+						$mergevars_val[$key] = $mailchimp_mergevars[$value]; 
+					}else{
+						$mergevars_val[$key] = ""; // en caso de que el input no llegara lo marca como vació
+					}
+				}
+
+			}
+			mailchimp($mergevars_val, $cabeza); // función para enviar a mailchimp 
+		}
+		return '¡Gracias!';	
 	}else{
 		return "Error en envío";
 	}
 }
 
+//----------------------------funciones para el mailchimp --------------------------------
 
-function mailchimp($mail_suscriptor, $cabeza){ // función desestabilizada temporalmente  
-	$apiKey = '4104c5726ff76b0a724c56eefd56e1ac';
-	$listId = '9b155e6de6';
+function merge_vars_gen($variable){ // genera los campos extra en caso del uso del mailchimp
+
+	$string=merge_vars_string($variable,id_form); // obtiene un string con todas las llaves y las variables 
+
+	$array_simple=explode('/fin/',$string); // obtiene un array con cada llave y su variable
+
+	foreach ($array_simple as  $key_array) {
+		if(!empty($key_array)){//Solo no vacias 
+			$key_y_array= explode('/sep/',$key_array);
+			//$salida[]=$key_y_array;
+			if(count($key_array)<>2){//solo los array que se encuentren bien 
+				$salida[$key_y_array[0]]=$key_y_array[1];
+			}
+
+		}
+	}
+
+	return $salida;
+}	
+
+function merge_vars_string($variable,$prehol){ // genera una cadena a partir de la cadena enviada por el form para obtener los los campos extra en caso del uso del mailchimp 
+	if(!empty($variable)){
+		if(is_array($variable)){
+				$salida=""; // escribe la primer linea 
+				foreach ( $variable as $key => $value ) { // escribe las key para como encabezados
+					if(!is_array($value)){ // solo escribe la key en caso de que no sea array
+						$salida.=$prehol.'['.$key.']/sep/';
+					}
+					$salida.=merge_vars_string($value,$prehol."[".$key."]");
+				}				
+		}else{ // escribe en contenido de cada input 
+			$salida=$variable.'/fin/';
+		}
+	}else{ // en caso de que el input se enviara vacío
+		$salida=''.'/fin/';
+	}
+	return $salida;
+}
+
+
+function mailchimp($mailchimp_mergevars, $cabeza){ // función de registro en mailchimp 
+
+	/*----------------------------parámetros del mailchimp----------------------------------*/
+	
+	$apiKey = mailchip_apikey; // apikey de mail chimp 
+	$listId = mailchip_listid; // id de la lista de mail chimp 
+	$submit_url = mailchip_listurl; // dirección de la lista en mail chimp 
 	$double_optin=false;
 	$send_welcome=false;
 	$email_type = 'html';
-	$email = $mail_suscriptor;
-	$submit_url = "http://us7.api.mailchimp.com/1.3/?method=listSubscribe";
-	$data = array(
-	    'email_address'=>$email,
+	
+	$data = array( //array de parametros  
 	    'apikey'=>$apiKey,
 	    'id' => $listId,
 	    'double_optin' => $double_optin,
 	    'send_welcome' => $send_welcome,
 	    'email_type' => $email_type
 	);
+	
+	/*-----------------------ingreso de los datos------------------------------------------------*/
+	
+	$data['email_address']=reg_mail; //definición del mail que se inscribe
+	if(mailchip_mergevar_on==true){
+		$data['merge_vars']= $mailchimp_mergevars; // definición del resto de los campos 
+	}
+	
+	
+	/*--------------- codigo de envio a mailchimp----------------------*/
 	$payload = json_encode($data);
 	 
 	$ch = curl_init();
@@ -91,15 +165,25 @@ function mailchimp($mail_suscriptor, $cabeza){ // función desestabilizada tempo
 	 
 	$result = curl_exec($ch);
 	curl_close ($ch);
-	$data = json_decode($result);
-	if ($data->error){
-		if($data->code != '214'){
-			$mail_cultivo = 'hola@elcultivo.mx';
-			$mailchimp_mensaje = "Hubo un error al tratar de registrar un mail a la lista.\r\nCódigo de error: ".$data->code;
-			mail($mail_cultivo, "Error Mailchimp", $mailchimp_mensaje, $cabeza);
+
+	/*---------------detección de errores y envió de los mismos-------------------*/
+	if($result != true){
+		if (mailchip_errors_send == true ){ // solo en caso de que quieran recibirse los errores por mail
+			$data = json_decode($result);
+			if ($data->error){
+				if($data->code != '214'){
+						$mail_cultivo = mail_errors_send; // mail para el reporte de errores 
+						$mailchimp_mensaje = "Hubo un error al tratar de registrar un mail a la lista. (".$listId.") \r\nCódigo de error: ".$data->code;
+						mail($mail_cultivo, "Error Mailchimp", $mailchimp_mensaje, $cabeza);
+				}else{
+					// en caso de mail ya inscrito
+				} 
+			}
 		}
-	    
-	} 
+	}else{
+		// en caso de correcto
+	}
+
 }
 
 ?>
--- ajax/ajax-mail_config.php
+++ ajax/ajax-mail_config.php
@@ -5,14 +5,41 @@ define('mail', 'contacto@contacto.mx'); //  mail donde se registran
 
 
 /*-------------------- es necesario repetir y modificar las siguientes lineas para cada form que contenga la pagina ------------*/ 
-$id_form = 'contact_form'; // cambiar el identificador del form
+$id_form = ''; // cambiar el identificador del form
 if(isset( $_POST[$id_form]) ){	
-		/*--------------------- identificadores del form -------------------------------------*/
-		define('id_form', 'contact_form');  
-		define('reg_name', $_POST[$id_form]['Nombre(s)']); // cambiar el identificador del nombre de la persona que envía en caso de no contar con este campo marcarlo como ''
-		define('reg_mail', $_POST[$id_form]['E-mail']); // cambiar el identificador del mail de la persona que suscribe  
-		/*--------------------- contenido del correo -------------------------------------*/
-		define('primera_linea', '<strong> Información de Contacto </strong><br>'); // cambiar el primera linea del contenido del correo 
+
+		if(defined('id_form')){ // detiene el código en caso de que ingrese información de dos form diferentes 
+			$break_form=true;
+		}else{
+			/*--------------------- identificadores del form -------------------------------------*/
+			define('id_form', $id_form);  
+			define('reg_name', $_POST[$id_form]['']); // cambiar el identificador del nombre de la persona que envía en caso de no contar con este campo marcarlo como ''
+			define('reg_mail', $_POST[$id_form]['']); // cambiar el identificador del mail de la persona que suscribe  
+			
+			/*--------------------- contenido del correo -------------------------------------*/
+			define('primera_linea', '<strong> Información de Contacto </strong>'); // cambiar el primera linea del contenido del correo 
+			
+			/*----------------------uso de mail chimp----------------------------------------*/
+			define('mailchip', false); // cambiar a true en caso de uso del mailchimp
+			if (mailchip == true ){
+				define('mailchip_apikey', ''); // cambiar el apy key de mailchimp
+				define('mailchip_listid', ''); // cambiar el list id de mailchimp
+				define('mailchip_listurl', ''); // cambiar el list id de mailchimp
+				define('mailchip_mergevar_on', false); // cambiar a true en caso de que se cuente con campos extra
+				if (mailchip_mergevar_on == true){
+					//permite tener mas input en el form para seleccionar solo los indispensables para mailchimp o cambiar así como depurar los input no enviados
+					$mailchip_merge_array = array( // cambiar la clave y el valor en función del tag de mailchimp y el nombre del input 
+												'MERGE1' => id_form.'[]', 
+												'MERGE2' => id_form.'[]'
+												); 
+				}
+				define('mailchip_errors_send', false); // cambiar a true en caso de que quieras reporte de errores de mailchimp por mail 
+				if (mailchip_errors_send == true ){
+					define('mail_errors_send', ''); // cambiar correo donde se envían los errores del mailchimp
+				}
+			}
+		}
+
 }
 unset($id_form);
 /*--------------------- repetir hasta aquí -------------------------*/
